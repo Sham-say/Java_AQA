4411f6c1dafac150238a5fd7dc79b749a686b9cb
Shamil <shoma99219@gmail.com>
2022-12-13 01:15:53 +0300

    1.5. Типы переменных и значений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,12 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
 
-    int l = 8;
-    int s = l * l;
+    //Переменная String - строка
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
+
+    /* Переменные
+    int - целочисленные
+    double - число с плавующей точкой (число двойной точности) */
+    double l = 8;
+    double s = l * l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
-    
   }
 
 }
