ce5f96883b20a72a07fc24536320b527c352a00e
Shamil <shoma99219@gmail.com>
2022-12-14 01:01:08 +0300

    Задание №2, Задачи 1, 2, 3.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramF.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramF.java
@@ -11,10 +11,25 @@ public class MyFirstProgramF {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со стооронами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point();
+    p1.x = 5;
+    p1.y = 10;
+    Point p2 = new Point();
+    p2.x = 10;
+    p2.y = 15;
+
+    distance(p1,p2);
+
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
+  public static void hello(String somebody){
+    System.out.println("Hello " + somebody + "!");
   }
 
+  public static void distance(Point p1, Point p2){
+    double d = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    System.out.println("Расстояние между двумя точками: " + d);
   }
+}
+
