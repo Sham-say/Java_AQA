b9562c625f51710632b4f54db809d066fbaff025
Shamil <shoma99219@gmail.com>
2022-12-14 10:27:47 +0300

    Доработка 4ой задачи.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramF.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramF.java
@@ -12,9 +12,10 @@ public class MyFirstProgramF {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со стооронами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point point = new Point(10, 17, 25, 15);
+    Point p1 = new Point(10,17);
+    Point p2 = new Point(16,20);
 
-    System.out.println("Расстояние между точками " + "p1" + "("+point.x1 + ":" + point.y1 + ")" + " и " + "p2" + "(" + point.x2 + ":" + point.y2 + ")" + " =" + point.distance());
+    System.out.println("Расстояние между точками " + "p1" + "("+p1.x + ":" + p1.y + ")" + " и " + "p2" + "(" + p2.x + ":" + p2.y + ")" + " =" + p1.distance(p2, p1));
 
   }
 
