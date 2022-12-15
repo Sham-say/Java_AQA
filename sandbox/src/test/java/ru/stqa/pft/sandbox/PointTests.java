c81f1e86d8c7f0ed88f9f1a81cb8f3a0493112a8
Shamil <shoma99219@gmail.com>
2022-12-15 13:38:35 +0300

    Мелкие изменения

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,12 +3,12 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PoinTests {
+public class PointTests {
   @Test
   public void testDistance() {
     Point p1 = new Point(10,17);
     Point p2 = new Point(16,20);
     Assert.assertEquals(p1.distance(p2), 6.708203932499369);
   }
-  
+
 }
