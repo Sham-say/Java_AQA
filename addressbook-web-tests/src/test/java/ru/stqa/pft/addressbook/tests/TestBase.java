4871f27c392320c0df042d754ec5d60112091161
Shamil <shoma99219@gmail.com>
2022-12-23 20:07:45 +0300

    Незначительные правки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-	protected final ApplicationManager app = new ApplicationManager(Browser.FIREFOX);
+	protected final ApplicationManager app = new ApplicationManager(Browser.EDGE);
 
 	@BeforeMethod
 	public void setUp() throws Exception {
