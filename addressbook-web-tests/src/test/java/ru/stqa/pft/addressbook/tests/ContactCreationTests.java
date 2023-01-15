950eef0ab3019e27e0df0adfa2a94842718fa2d9
Shamil <shoma99219@gmail.com>
2023-01-16 00:54:18 +0300

    Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,13 +26,7 @@ public class ContactCreationTests extends TestBase {
 		List<ContactData> after = app.getContactHelper().getContactList();
 		Assert.assertEquals(after.size(), before.size() + 1);
 
-		int max = 0;
-		for (ContactData c : after) {
-			if (c.getId() > max) {
-				max = c.getId();
-			}
-		}
-		contact.setId(max);
+		contact.setId(after.stream().max(Comparator.comparingInt(ContactData::getId)).get().getId());
 		before.add(contact);
 		Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 	}
