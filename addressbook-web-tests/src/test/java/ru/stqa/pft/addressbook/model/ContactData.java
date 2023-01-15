6547945741b6cbfbd02fedda035be1af45712cff
Shamil <shoma99219@gmail.com>
2023-01-16 01:36:55 +0300

    Задание №9: Реализовать проверки путём сравнения списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -22,8 +22,9 @@ public class ContactData {
 		this.group = group;
 	}
 
-	public ContactData(String firstName, String lastName,String address, String email, String home, String group) {
-		this.id = 0;
+
+	public ContactData(String firstName, String lastName, String address, String email, String home, String group) {
+		this.id = Integer.MAX_VALUE;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
@@ -66,31 +67,29 @@ public class ContactData {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		ContactData that = (ContactData) o;
-
-		if (id != that.id) return false;
-		if (!Objects.equals(firstName, that.firstName)) return false;
-		return Objects.equals(lastName, that.lastName);
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id;
-		result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-		result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-		return result;
-	}
-
-	@Override
 	public String toString() {
 		return "ContactData{" +
 				"id='" + id + '\'' +
 				", firstName='" + firstName + '\'' +
 				", lastName='" + lastName + '\'' +
 				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		ContactData that = (ContactData) o;
+
+		if (!Objects.equals(firstName, that.firstName)) return false;
+		return Objects.equals(lastName, that.lastName);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = firstName != null ? firstName.hashCode() : 0;
+		result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+		return result;
 	}
 }
