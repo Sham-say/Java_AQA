1b6e3ad18cb4a9a12a32ae8c910065226472efe3
Shamil <shoma99219@gmail.com>
2023-01-24 23:25:28 +0300

    6.6. Формат XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,7 +6,7 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-	protected static final ApplicationManager app = new ApplicationManager(Browser.FIREFOX);
+	protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME);
 
 	@BeforeSuite
 	public void setUp() throws Exception {
