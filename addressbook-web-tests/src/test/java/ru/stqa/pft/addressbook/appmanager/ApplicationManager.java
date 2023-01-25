2dc9434711f9b1c027263009eac4f4a54216f722
Shamil <shoma99219@gmail.com>
2023-01-26 00:40:53 +0300

    Задание №14: Вынести описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,54 +1,56 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 
-import java.util.Objects;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
 public class ApplicationManager {
-	public WebDriver wd;
+	private final Properties properties;
+	WebDriver wd;
 
-	private ContactHelper contactHelper;
 	private SessionHelper sessionHelper;
 	private NavigatorHelper navigatorHelper;
 	private GroupHelper groupHelper;
-	//переменные для логина и пароля
-	String userLogin = "admin";
-	String userPassword = "secret";
-	private JavascriptExecutor js;
-	private Browser browser;
+	private String browser;
+	private ContactHelper contactHelper;
 
-
-	public ApplicationManager(Browser browser) {
+	public ApplicationManager(String browser)  {
 		this.browser = browser;
+		properties = new Properties();
 	}
 
-	public void init() {
-		if (Objects.equals(browser, Browser.FIREFOX)) {
-			wd = new FirefoxDriver(new FirefoxOptions().setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
-		} else if (Objects.equals(browser, Browser.CHROME)) {
+	public void init() throws IOException {
+		String target = System.getProperty("target", "local");
+
+		properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+		if (browser.equals(BrowserType.FIREFOX)) {
+			wd = new FirefoxDriver();
+		} else if (browser.equals( BrowserType.CHROME)) {
 			wd = new ChromeDriver();
-		} else if (Objects.equals(browser, Browser.EDGE)) {
+		} else if (browser.equals( BrowserType.EDGE)) {
 			wd = new EdgeDriver();
 		}
 		wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-		wd.get("http://localhost/addressbook");
-		contactHelper = new ContactHelper(wd);
+		wd.get(properties.getProperty("web.baseUrl"));
 		groupHelper = new GroupHelper(wd);
+		contactHelper = new ContactHelper(wd);
 		navigatorHelper = new NavigatorHelper(wd);
 		sessionHelper = new SessionHelper(wd);
-		sessionHelper.login(userLogin, userPassword);
+		sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
 	}
 
-
 	public void stop() {
-		sessionHelper.logOut(userLogin, userPassword);
 		wd.quit();
 	}
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-	protected static final ApplicationManager app = new ApplicationManager(Browser.CHROME);
+	protected static final ApplicationManager app
+			= new ApplicationManager(System.getProperty("browser", String.valueOf(BrowserType.CHROME)));
 
 	@BeforeSuite
 	public void setUp() throws Exception {
