370a9b6a5aed8ac97978a11f62fe6be5499be20c
Shamil <shoma99219@gmail.com>
2023-01-26 21:17:05 +0300

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,38 +3,65 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contacts")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
 	@XStreamOmitField
+	@Id
+	@Column(name = "id")
 	private int id = Integer.MAX_VALUE;
 	@Expose
+	@Column(name = "firstname")
 	private String firstName;
 	@Expose
+	@Column(name = "lastname")
 	private String lastName;
 	@Expose
+	@Column(name = "address")
+	@Type(type = "text")
 	private String address;
+	@Column(name = "email")
+	@Type(type = "text")
 	private String email;
+	@Column(name = "email2")
+	@Type(type = "text")
 	private String email2;
+	@Column(name = "email3")
+	@Type(type = "text")
 	private String email3;
+	@Transient
 	private String allEmail;
+	@Column(name = "home")
+	@Type(type = "text")
 	private String homePhone;
+	@Transient
 	private String group;
+	@Column(name = "mobile")
+	@Type(type = "text")
 	private String mobilePhone;
+	@Column(name = "work")
+	@Type(type = "text")
 	private String workPhone;
 	@Expose
+	@Transient
 	private String allPhone;
-	private File photo;
+	@Column(name = "photo")
+	@Type(type = "text")
+	private String photo;
 
 	public File getPhoto() {
-		return photo;
+		return new File(photo);
 	}
 
 	public ContactData withPhoto(File photo) {
-		this.photo = photo;
+		this.photo = photo.getPath();
 		return this;
 	}
 
@@ -121,7 +148,6 @@ public class ContactData {
 				", firstName='" + firstName + '\'' +
 				", lastName='" + lastName + '\'' +
 				", address='" + address + '\'' +
-				", allPhone='" + allPhone + '\'' +
 				'}';
 	}
 
